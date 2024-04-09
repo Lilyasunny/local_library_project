2ce0607b011852b6c261cea70d97b0caf5497278
Lilya <masya@Lilyas-MBP.localdomain>
2024-04-09 18:25:57 -0400

    Complete qualified assignment, ...

---
+++ public/src/home.js
@@ -0,0 +1,335 @@
+// const books = [
+//   {
+//     id: "5f447132d487bd81da01e25e",
+//     title: "sit eiusmod occaecat eu magna",
+//     genre: "Science",
+//     authorId: 8,
+//     borrows: [
+//       {
+//         id: "5f446f2e2cfa3e1d234679b9",
+//         returned: false,
+//       },
+//       {
+//         id: "5f446f2ed3609b719568a415",
+//         returned: true,
+//       },
+//     ],
+//   },
+//   {
+//     id: "5f4471329627160be1e8ce92",
+//     title: "esse ea veniam non occaecat",
+//     genre: "Classics",
+//     authorId: 10,
+//     borrows: [
+//       {
+//         id: "5f446f2ed3609b719568a415",
+//         returned: false,
+//       },
+//       {
+//         id: "5f446f2ec32d71dabec35b06",
+//         returned: true,
+//       },
+//     ],
+//   },
+//   {
+//     id: "5f44713265e5d8d17789beb0",
+//     title: "tempor occaecat fugiat",
+//     genre: "Travel",
+//     authorId: 16,
+//     borrows: [
+//       {
+//         id: "5f446f2e4eff1030e7316861",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2ecc5c4787c403f844",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2ee1661e64cde14e55",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e50cc2da9cd80efdb",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2ef795e593cd3cd19d",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2eae901a82e0259947",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e3e70bb4e1ab821c9",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2ef2ab5f5a9f60c4f2",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e136866e4fe60c893",
+//         returned: true,
+//       },
+//     ],
+//   },
+//   {
+//     id: "5f44713264bb872240dd62d0",
+//     title: "proident cupidatat fugiat aliquip do",
+//     genre: "Young Adult",
+//     authorId: 20,
+//     borrows: [
+//       {
+//         id: "5f446f2efa7fe184c4014dd2",
+//         returned: false,
+//       },
+//       {
+//         id: "5f446f2e59f9380a1d03d766",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2ede05a0b1e3394d8b",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2ed9aac23c0340aab2",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e136866e4fe60c893",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2eef419207c5fa4ec9",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e409f8883af2955dd",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e59f9380a1d03d766",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e7d3e00a5efe35b6e",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2ec32d71dabec35b06",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e3e70bb4e1ab821c9",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e5abedb2ba5eecd16",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e7eb4accbc92cb7a8",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2ef7fc711c78b57e51",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e5a23764338868b10",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2ea0502bf8cbc7676a",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2eaf32d8ddf09e8f83",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2eef419207c5fa4ec9",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e91a138c3a8ac6980",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e5e2952040e9f9b88",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e141b97d842b680fd",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2ee734f727dfceefbd",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2efa2e5110917e5929",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e5e2952040e9f9b88",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e4081699cdc6a2735",
+//         returned: true,
+//       },
+//       {
+//         id: "5f446f2e7b9cd304fed3a8bc",
+//         returned: true,
+//       },
+//     ],
+//   }]
+
+const accounts = [
+  {
+    id: "5f446f2ecfaf0310387c9603",
+    picture: "https://api.adorable.io/avatars/75/esther.tucker@zillacon.me",
+    age: 25,
+    name: {
+      first: "Esther",
+      last: "Tucker",
+    },
+    company: "ZILLACON",
+    email: "esther.tucker@zillacon.me",
+    registered: "Thursday, May 28, 2015 2:51 PM",
+  },
+  {
+    id: "5f446f2ed46724f41c9fc431",
+    picture: "https://api.adorable.io/avatars/75/ferrell.morris@ecolight.com",
+    age: 35,
+    name: {
+      first: "Ferrell",
+      last: "Morris",
+    },
+    company: "ECOLIGHT",
+    email: "ferrell.morris@ecolight.com",
+    registered: "Thursday, February 8, 2018 1:16 PM",
+  },
+  {
+    id: "5f446f2e5e2952040e9f9b88",
+    picture: "https://api.adorable.io/avatars/75/robertson.simmons@goko.tv",
+    age: 38,
+    name: {
+      first: "Robertson",
+      last: "Simmons",
+    },
+    company: "GOKO",
+    email: "robertson.simmons@goko.tv",
+    registered: "Monday, October 30, 2017 10:28 PM",
+  }]
+
+function getTotalBooksCount(books) {
+  // YOUR SOLUTION HERE
+  let totalBooks = 0
+  for (let i = 0; i <= books.length; i++) {
+    totalBooks = i;
+  }
+  return totalBooks;
+}
+
+
+function getTotalAccountsCount(accounts) {
+  // YOUR SOLUTION HERE
+  let result = accounts.length;
+  return result;
+}
+
+function getBooksBorrowedCount(books) {
+  let borrowedBooks = books.filter((book) => {
+    return book.borrows.some((borrow) => !borrow.returned);
+  });
+  return borrowedBooks.length;
+}
+
+
+
+// Tbis is a helper function that's called by other functions inside this file. You don't have to edit it.
+function _sortObjectByValues(obj) {
+  const keys = Object.keys(obj);
+  return keys.sort((keyA, keyB) => {
+    if (obj[keyA] > obj[keyB]) {
+      return -1;
+    } else if (obj[keyB] > obj[keyA]) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getMostCommonGenres(books) {
+  const count = books.reduce((acc, { genre }) => {
+    if (acc[genre]) {
+      acc[genre] += 1;
+    } else {
+      acc[genre] = 1;
+    }
+
+    return acc;
+  }, {});
+
+  const sorted = _sortObjectByValues(count);
+  return sorted.map((name) => ({ name, count: count[name] })).slice(0, 5);
+}
+
+function getMostPopularBooks(books) {
+  const groupById = books.reduce((acc, { id, borrows }) => {
+    acc[id] = borrows.length;
+    return acc;
+  }, {});
+
+  const sorted = _sortObjectByValues(groupById);
+  return sorted
+    .map((id) => {
+      const { title: name } = books.find(({ id: bookId }) => bookId === id);
+      return { name, count: groupById[id] };
+    })
+    .slice(0, 5);
+}
+
+function getMostPopularAuthors(books, authors) {
+  const count = books.reduce((acc, { authorId, borrows }) => {
+    if (acc[authorId]) {
+      acc[authorId].push(borrows.length);
+    } else {
+      acc[authorId] = [borrows.length];
+    }
+
+    return acc;
+  }, {});
+
+  for (let id in count) {
+    const sum = count[id].reduce((a, b) => a + b);
+    count[id] = sum;
+  }
+
+  const sorted = _sortObjectByValues(count);
+  return sorted
+    .map((authorId) => {
+      const {
+        name: { first, last },
+      } = authors.find(({ id }) => id === Number(authorId));
+      const name = `${first} ${last}`;
+      return { name, count: count[authorId] };
+    })
+    .slice(0, 5);
+}
+
+module.exports = {
+  getTotalBooksCount,
+  getTotalAccountsCount,
+  getBooksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
+
